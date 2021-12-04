10b5f58b54084e5147a2676d4ea349d0a64d7322
Adil Permata Sidik <adilpermatasidik@gmail.com>
2021-12-04 12:16:38 +0700

    Create 404.tsx

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404(){
+    return(
+        <div>
+            <h1 className="title-not-found">Shit Man</h1>
+            <h1 className="title-not-found">No Page here</h1>
+        </div>
+    )
+}
