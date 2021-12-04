1d9644494802462ab3f1c0342969cf7fc2810ad9
Adil Permata Sidik <adilpermatasidik@gmail.com>
2021-12-04 14:59:46 +0700

    Update index.tsx

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,19 +1,26 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 import Footer from "../Footer";
 import Header from "../Header";
 import styles from './Layout.module.css';
-
+import Head from 'next/head';
 interface LayoutProps{
     children : ReactNode;
+    pageTitle : string;
 }
 
 export default function Layout(props: LayoutProps){
-    const { children } = props;
+    const { children, pageTitle } = props;
     return (
+        <>
+        <Head>
+            <title> NextJS Basic | {pageTitle} </title>
+            <meta name="description" content="Website Nextjs Basic" />
+        </Head>
         <div className={styles.container}>
             <Header />
             <div className={styles.content}>{children}</div>
             <Footer />
         </div>
+        </>
     )
 }
